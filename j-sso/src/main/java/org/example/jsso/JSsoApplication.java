a9f6f1ff376f07104814b9f79659f22e5d38b55c
Мирослав Косюк <87585769+Baldezxniy@users.noreply.github.com>
2024-02-08 02:58:49 +0200

    Create simple authorization server configuration

---
+++ j-sso/src/main/java/org/example/jsso/JSsoApplication.java
@@ -0,0 +1,13 @@
+package org.example.jsso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JSsoApplication.class, args);
+	}
+
+}
---
+++ j-sso/src/test/java/org/example/jsso/JSsoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jsso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JSsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

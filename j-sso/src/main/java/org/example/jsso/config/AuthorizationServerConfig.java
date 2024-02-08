7e7ac093fd33ba454ed80761906b20b8d8cb16f5
Мирослав Косюк <87585769+Baldezxniy@users.noreply.github.com>
2024-02-08 22:10:15 +0200

    #1 Migrated from jwt to opaque token

--- j-sso/src/main/java/org/example/jsso/config/AuthorizationServerConfig.java
+++ j-sso/src/main/java/org/example/jsso/config/AuthorizationServerConfig.java
@@ -1,11 +1,6 @@
 package org.example.jsso.config;
 
-import com.nimbusds.jose.jwk.JWKSet;
-import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
-import org.example.jsso.utils.JwkUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -18,8 +13,13 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 @RequiredArgsConstructor
 @Configuration(proxyBeanMethods = false)
@@ -46,26 +46,35 @@ public class AuthorizationServerConfig {
 						.clientName("Test Client")
 						.clientId("test-client")
 						.clientSecret("{noop}test-client")
-						.redirectUri("http://localhost:5000/code")
+						.redirectUri("http://127.0.0.1:3000/code")
+						.scope("read.scope")
+						.scope("write.scope")
 						.clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
 						.authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
 						.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
 						.authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+						.tokenSettings(TokenSettings.builder()
+								// Access token format
+								.accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+								// Access token live time
+								.accessTokenTimeToLive(Duration.of(30, ChronoUnit.MINUTES))
+								// Refresh token live time
+								.refreshTokenTimeToLive(Duration.of(120, ChronoUnit.MINUTES))
+								//  Allowed to reuse the refresh_token again if it has not yet expired
+								.reuseRefreshTokens(false)
+								// authorize_code live time
+								.authorizationCodeTimeToLive(Duration.of(30, ChronoUnit.SECONDS))
+								.build()
+						)
 						.build()
 		);
-	}
-
-	@Bean
-	public JWKSource<SecurityContext> jwkSource() {
-		RSAKey rsaKey = JwkUtils.generateRsa();
-		JWKSet jwkSet = new JWKSet(rsaKey);
-		return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
 	}
 
 	@Bean
 	public AuthorizationServerSettings authorizationServerSettings() {
 		return AuthorizationServerSettings.builder()
 				.issuer(authorizationServerProperties.getIssuerUrl())
+				.tokenIntrospectionEndpoint(authorizationServerProperties.getIntrospectionEndpoint())
 				.build();
 	}
 }
